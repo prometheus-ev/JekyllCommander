prometheus-ev/JekyllCommander

commit 09aff63aa11e4351d82eebb0d6d436bbfde095c3

Beginning support for page types. Added optional link to user.

lib/helpers.rb
@@ -113,6 +113,11 @@ module JekyllCommander; module Helpers
     }.merge(html_options))
   end
 
+  def link_to_user(name = user, html_options = {})
+    link = user_config[:link]
+    link ? link_to(name, link, html_options) : name
+  end
+
   def flash(hash)
     session[:flash] = {} unless session[:flash].is_a?(Hash)
     flash = session[:flash]
@@ -230,6 +235,7 @@ module JekyllCommander; module Helpers
 
     @real_path = real_path(@path)
     @base = File.basename(@path)
+    @type = Page.type(@path)
 
     @dir  = @base if File.directory?(@real_path)
     @file = @base if File.file?(@real_path)
@@ -260,6 +266,18 @@ module JekyllCommander; module Helpers
     end
   end
 
+  def user_config
+    @user_config ||= options.users && options.users[user] || {}
+  end
+
+  def user_name
+    @user_name ||= user_config[:name] || user
+  end
+
+  def user_email
+    @user_email ||= user_config[:email] || options.email % user
+  end
+
   def repo_name
     @repo_name ||= File.basename(options.repo, '.git')
   end
@@ -279,12 +297,8 @@ module JekyllCommander; module Helpers
       base, name = File.split(repo_root)
       git = Git.clone(options.repo, name, :path => base, :bare => false)
 
-      if options.users and config = options.users[user]
-        name, email = config.values_at(:name, :email)
-      end
-
-      git.config('user.name',  name  || user)
-      git.config('user.email', email || options.email % user)
+      git.config('user.name',  user_name)
+      git.config('user.email', user_email)
 
       rake
     end
@@ -466,6 +480,14 @@ module JekyllCommander; module Helpers
     }
 
     matches
+  end
+
+  def page
+    return @page if defined?(@page)
+
+    @page = Page.load(repo_root, @path)
+    flash :error => "Unable to load page `#{@base}'." unless @page
+    @page
   end
 
 end; end

lib/page.rb
@@ -15,6 +15,15 @@ module JekyllCommander; class Page
     end
   }
 
+  TYPE = Hash.new(:page).merge(
+    '_includes'   => :include,
+    '_layouts'    => :layout,
+    '_posts'      => :post,
+    'files'       => :file,
+    'javascripts' => :javascript,
+    'stylesheets' => :stylesheet
+  )
+
   LANGUAGES = %w[en de]
   DEFAULT_LANGUAGE = LANGUAGES.first
 
@@ -45,17 +54,23 @@ module JekyllCommander; class Page
 
   EXT_RE = %r{(?:\.(#{LANGUAGES.join('|')}))?(?:\.(\w+))?\z}
 
-  def self.lang(path)
+  class << self
+
+  def type(path)
+    TYPE[path.sub(%r{\A/}, '').sub(%r{/.*}, '')]
+  end
+
+  def lang(path)
     path =~ EXT_RE
     LANGUAGES.include?($1) ? $1 : DEFAULT_LANGUAGE
   end
 
-  def self.load(path)
-    return unless File.file?(path)
+  def load(root, path)
+    return unless File.file?(File.join(root, path))
 
     base, name = File.split(path)
 
-    new(base, nil, [
+    new(root, base, nil, [
       [:lang,      lang(name)],
       [:slug,      name.sub(EXT_RE, '')],
       [:ext,       $2],
@@ -64,11 +79,13 @@ module JekyllCommander; class Page
     ]).load
   end
 
-  attr_accessor :base, :title, :slug, :ext, :markup, :render,
-                :lang, :multilang, *TRANSLATED_ATTRS
+  end
 
-  def initialize(base, title = nil, options = {})
-    @base, @title, @errors = base, title, []
+  attr_accessor :root, :base, :title, :slug, :ext, :markup,
+                :render, :lang, :multilang, *TRANSLATED_ATTRS
+
+  def initialize(root, base, title = nil, options = {})
+    @root, @base, @title, @errors = root, base.sub(%r{\A/}, ''), title, []
 
     # this one is essential, so we set it first
     @lang = Array(options.to_a.assoc(:lang)).last || DEFAULT_LANGUAGE
@@ -81,7 +98,7 @@ module JekyllCommander; class Page
       next unless exist?(lang)
 
       if markup?
-        body, header = PageFile.read_yaml(base, filename(lang))
+        body, header = PageFile.read_yaml(basepath, filename(lang))
 
         if header.empty?
           header = nil
@@ -95,7 +112,7 @@ module JekyllCommander; class Page
         if ext == 'yml'
           header = YAML.load_file(fullpath(lang))
         elsif ext == 'xml'
-          body, header = PageFile.read_yaml(base, filename(lang))
+          body, header = PageFile.read_yaml(basepath, filename(lang))
         else
           body = File.read(fullpath(lang))
         end
@@ -248,8 +265,12 @@ module JekyllCommander; class Page
     name
   end
 
+  def basepath
+    @basepath ||= File.join(root, base)
+  end
+
   def fullpath(lang = lang)
-    File.join(base, filename(lang))
+    File.join(basepath, filename(lang))
   end
 
   def valid?
@@ -271,7 +292,7 @@ module JekyllCommander; class Page
       File.exist?(fullpath(lang))
     else
       translated { |lang| return true if File.exist?(fullpath(lang)) }
-      File.exist?(File.join(base, slug)) unless slug.blank?
+      File.exist?(File.join(basepath, slug)) unless slug.blank?
     end
   end
 
@@ -300,6 +321,10 @@ module JekyllCommander; class Page
     else
       languages.each { |lang| yield lang }
     end
+  end
+
+  def type
+    @type ||= self.class.type(base)
   end
 
 end; end

lib/routes.rb
@@ -32,7 +32,12 @@ module JekyllCommander; module Routes
   end
 
   get '/*;new_:type' do
-    erb :"new_#{params[:type]}"
+    begin
+      erb :"new_#{params[:type]}"
+    rescue Errno::ENOENT
+      flash :error => "Type `#{params[:type]}' not supported yet."
+      erb :index
+    end
   end
 
   get '/*;status' do
@@ -133,8 +138,8 @@ module JekyllCommander; module Routes
   end
 
   post '/*;search' do
-    @query, @type = params[:query], params[:type]
-    @matches = search(@query, @type) || []
+    @query, @query_type = params[:query], params[:type]
+    @matches = search(@query, @query_type) || []
 
     erb :search
   end
@@ -157,29 +162,26 @@ module JekyllCommander; module Routes
   end
 
   put '/*' do
-    if @page = Page.load(@real_path)
-      if @page.update(real_params, Page.lang(@path))
-        name = @page.filename
+    return erb(:index) unless page
 
-        if @page.write!(git)
-          flash :notice => "Page `#{name}' successfully updated."
+    if page.update(real_params, Page.lang(@path))
+      name = page.filename
 
-          if name != @base
-            delete_page(name)  # delete old page, redirect to new one
-            return
-          end
-        else
-          flash :error => "Unable to write page `#{name}'."
+      if page.write!(git)
+        flash :notice => "Page `#{name}' successfully updated."
+
+        if name != @base
+          delete_page(name)  # delete old page, redirect to new one
+          return
         end
       else
-        flash :error => @page.errors
+        flash :error => "Unable to write page `#{name}'."
       end
-
-      erb :edit
     else
-      flash :error => "Unable to load page `#{@base}'."
-      erb :index
+      flash :error => page.errors
     end
+
+    erb :edit
   end
 
   delete '/*' do
@@ -197,23 +199,19 @@ module JekyllCommander; module Routes
 
   def preview_page
     return unless @file
+    return redirect(relative_url) unless page
 
-    if page = Page.load(@real_path)
-      path = relative_path(page.slug)
-      path = [page.lang, path] if page.multilang?
+    path = relative_path(page.slug)
+    path = [page.lang, path] if page.multilang?
 
-      preview(path, @action)
-    else
-      flash :error => "Unable to load page `#{@base}'."
-      redirect relative_url
-    end
+    preview(path, @action)
   end
 
   def render_folder
-    if @dir
-      chdir(@real_path)
-      erb :index
-    end
+    return unless @dir
+
+    chdir(@real_path)
+    erb :index
   end
 
   def render_page
@@ -221,13 +219,10 @@ module JekyllCommander; module Routes
 
     chdir(File.dirname(@real_path))
 
-    if @page = Page.load(@real_path)
-      flash :error => 'NOTE: This page has conflicts!!' if conflict?(@real_path)
-      erb :edit
-    else
-      flash :error => "Unable to load page `#{@base}'."
-      erb :index
-    end
+    return erb(:index) unless page
+
+    flash :error => 'NOTE: This page has conflicts!!' if conflict?(@real_path)
+    erb :edit
   end
 
   def create_folder
@@ -256,18 +251,18 @@ module JekyllCommander; module Routes
   end
 
   def create_page
-    @page = Page.new(@real_path, params[:title], [
+    @page = Page.new(repo_root, @path, params[:title], [
       [:multilang, !params[:multilang].nil?],
       [:render,    !params[:render].nil?],
       [:markup,    params[:markup]],
       [:layout,    params[:layout] || 'default']
     ])
 
-    if @page.write(git)
+    if page.write(git)
       flash :notice => "Page `#{@base}' successfully created."
-      redirect relative_url(@page.filename)
+      redirect relative_url(page.filename)
     else
-      flash :error => @page.errors
+      flash :error => page.errors
       get_files
 
       erb :new_page
@@ -283,7 +278,7 @@ module JekyllCommander; module Routes
   end
 
   def delete_page(new_name = nil)
-    if page = Page.load(@real_path)
+    if page
       if page.destroy(git)
         action = new_name ? 'renamed' : 'deleted'
         flash :notice => "Page `#{@base}' successfully #{action}."
@@ -293,8 +288,6 @@ module JekyllCommander; module Routes
       else
         flash :error => page.errors
       end
-    else
-      flash :error => "Unable to load page `#{@base}'."
     end
 
     erb :index

views/layout.erb
@@ -22,7 +22,7 @@
       <div id="loginfo">[
         <%= options.site ? link_to(repo_name, options.site) : repo_name %>
         <%= link_to_site %> <%= link_to_staging %> <%= link_to_preview %>
-        : <strong><%= user %></strong>
+        : <strong><%= link_to_user %></strong>
       ]</div>
 
       <div id="trail">
@@ -34,8 +34,8 @@
           <label for="search_query">Search:</label>
           <input type="text" name="query" id="search_query" value="<%= @query %>" />
           <select name="type" id="search_type">
-            <option value="name"<%= ' selected="selected"' if @type == 'name' %>>Name</option>
-            <option value="text"<%= ' selected="selected"' if @type == 'text' %>>Text</option>
+            <option value="name"<%= ' selected="selected"' if @query_type == 'name' %>>Name</option>
+            <option value="text"<%= ' selected="selected"' if @query_type == 'text' %>>Text</option>
           </select>
           <input type="submit" value="Go" />
         </form>
@@ -50,9 +50,9 @@
       </div>
 
       <div id="actions">
-        <%= link_to 'New page',   relative_path(';new_page') %> |
-        <%= link_to 'New folder', relative_path(';new_folder') %> |
-        <%= link_to 'Status',     relative_path(';status') %>
+        <%= link_to "New #{@type}", relative_path(";new_#{@type}") %> |
+        <%= link_to 'New folder',   relative_path(';new_folder') %> |
+        <%= link_to 'Status',       relative_path(';status') %>
 
       <% unless @file %>
         <br style="margin-bottom: 0.9em" />

views/edit.erb
@@ -1,4 +1,4 @@
-<h2>Edit page <code><%= @path %></code></h2>
+<h2>Edit <%= @page.type %> <code><%= @path %></code></h2>
 
 <p>
   <%= language_links(@page) if @page.multilang? %>

commit 8f885b80288099ade5207de3d2847c460fbe70b3

Fix renaming (broke in 09aff63). Downcase slug and use hyphen as separator.

lib/helpers.rb
@@ -487,11 +487,14 @@ module JekyllCommander
     end
 
     def page
-      return @page if defined?(@page)
+      defined?(@page) ? @page : @page = load_page
+    end
 
-      @page = Page.load(repo_root, @path)
-      flash :error => "Unable to load page `#{@base}'." unless @page
-      @page
+    def load_page
+      page = Page.load(repo_root, @path)
+      flash :error => "Unable to load page `#{@base}'." unless page
+
+      page
     end
 
   end

lib/page.rb
@@ -155,9 +155,9 @@ module JekyllCommander
     end
 
     def slug
-      @slug ||= @title && @title.replace_diacritics.
-        gsub(/[^a-zA-Z0-9]+/, '_').
-        gsub(/\A_+|_+\z/, '')
+      @slug ||= @title && @title.
+        replace_diacritics.downcase.
+        gsub(/\W+/, '-').gsub(/\A-|-\z/, '')
     end
 
     def title(lang = lang)

lib/routes.rb
@@ -280,7 +280,7 @@ module JekyllCommander
     end
 
     def delete_page(new_name = nil)
-      if page
+      if page = new_name ? load_page : page()
         if page.destroy(git)
           action = new_name ? 'renamed' : 'deleted'
           flash :notice => "Page `#{@base}' successfully #{action}."

views/layout.erb
@@ -60,7 +60,7 @@
       <% end %>
 
       <% if @path == '/' %>
-        <%= form_delete 'Discard copy!' %> |
+        <%= form_delete 'Reset copy!' %> |
         <%= form :post, '/' + u(';update') %>
           <p><input type="submit" value="Update copy" /></p>
         </form>