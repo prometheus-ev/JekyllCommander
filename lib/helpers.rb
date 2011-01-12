ed6274461058b3f349eb5776849051ef90dfd565
Jens Wille <jens.wille@uni-koeln.de>
2011-01-12 19:07:03 +0100

    lib/helpers.rb (ensure_repo): Mark repo as shared.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -340,6 +340,8 @@ module JekyllCommander
         git.config('user.name',  user_name)
         git.config('user.email', user_email)
 
+        git.config('core.sharedRepository', true)
+
         rake
       end
     end
